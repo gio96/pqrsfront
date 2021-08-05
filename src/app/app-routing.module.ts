f09ff94319ed3e4c1545ad0c7d7e4cc47d822112
Giovanny Gomez <giovanny.gomez@masglobalconsulting.com>
2021-08-04 22:35:44 -0500

    feat: solicitud reclamo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { SolicitudComponent } from './solicitud/solicitud.component';
 import { CrearSolicitudComponent } from './crear-solicitud/crear-solicitud.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetallesSolicitudComponent } from './detalles-solicitud/detalles-solicitud.component';
 
 const routes: Routes = [
   {
-    path: 'crear-solicitud', component: CrearSolicitudComponent
+    //path: 'crear-solicitud', component: CrearSolicitudComponent
+    path: 'crear-solicitud', component: DetallesSolicitudComponent
   },
   {
     path: 'solicitud', component: SolicitudComponent
