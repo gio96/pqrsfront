80dbceb759f7abc278eaef25ff688f6ebbf3b6cb
Giovanny Gomez <giovanny.gomez@masglobalconsulting.com>
2021-08-12 16:29:16 -0500

    feat: search button

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -11,13 +11,30 @@ export class SolicitudComponent implements OnInit {
 
   displayedColumns: string[] = ['radicado', 'fecha', 'descripcion', 'estado'];
   dataSource = ELEMENT_DATA;
+  valorBusqueda: String = ''
 
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   selectedRow(row: SolicitudGetModel) {
     this.router.navigate(['/detalles',row.id])
+  }
 
+  buscar(){
+
+    if(this.valorBusqueda.trim() !== ''){
+      this.dataSource = [
+        { id: '0dfgdfsd754sd1', fechaSolicitud: '04/08/2021', descripcionSolicitud: 'descripcion', respuestaAdministrativa: 'H' }
+      ]
+    }else{
+      this.dataSource = [
+        { id: '0dfgdfsd754sd1', fechaSolicitud: '04/08/2021', descripcionSolicitud: 'descripcion', respuestaAdministrativa: 'H' },
+        { id: '0dfgdfsd754sd2', fechaSolicitud: '30/01/2020', descripcionSolicitud: 'descripcion', respuestaAdministrativa: '' },
+        { id: '0dfgdfsd754sd3', fechaSolicitud: '04/08/2019', descripcionSolicitud: 'descripcion', respuestaAdministrativa: 'Li' },
+        { id: '0dfgdfsd754sd4', fechaSolicitud: '25/02/2021', descripcionSolicitud: 'descripcion', respuestaAdministrativa: 'Be' },
+        { id: '0dfgdfsd754sd5', fechaSolicitud: '04/08/2021', descripcionSolicitud: 'descripcion', respuestaAdministrativa: 'B' },
+      ];
+    }
   }
 
   ngOnInit(): void {
