bf26531948bd2117b7b8745a88bc6342de74ba0b
Giovanny <gio96gomez@gmail.com>
2021-08-14 10:54:55 -0500

    feat: table reclamos

--- src/app/reclamo/reclamo.component.ts
+++ src/app/reclamo/reclamo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SolicitudGetModel } from '../shared/models/solicitudModel';
+import { SolicitudService } from '../shared/servicios/solicitud.service';
 
 @Component({
   selector: 'app-reclamo',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReclamoComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['radicado','fecha', 'descripcion'];
+  dataSource: any = []
+  valorBusqueda: string = ''
+
+  constructor(private router: Router, private solicitudService: SolicitudService) { }
+
+  selectedRow(row: SolicitudGetModel) {
+    this.router.navigate(['/detalles',row.id],{state: row})
+  }
+
+  buscar(){
+    if(this.valorBusqueda.trim() !== ''){
+      this.getSolicitudId(this.valorBusqueda)
+    }else {
+      this.getAllSolicitudes();
+    }
+  }
 
   ngOnInit(): void {
+    this.getAllSolicitudes();
+  }
+
+  getAllSolicitudes() {
+    return this.solicitudService.getAllSolicitud().subscribe((data: {}) => {
+      this.dataSource = data
+    })
+  }
+
+  getSolicitudId(id: string) {
+    return this.solicitudService.getSolicitudById(id).subscribe((data: {}) => {
+      this.dataSource = [data]
+    })
   }
 
 }
